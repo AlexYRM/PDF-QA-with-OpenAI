ae347ef568d9e6d6b2ca7a8baae257bd72cea75e
AlexYRM <alexirimia31@gmail.com>
2023-04-02 13:50:54 +0300

    Changed uploading files and created asking questions and receiving AI generated answers

--- GUI.py
+++ GUI.py
@@ -1,45 +1,105 @@
-from nicegui import ui, events
+import AI
 import csv
 import time
 import datetime
+from nicegui import ui, events
 
+question = ""
+select = ""
+file_storage = []
+skippable = True
+updated = False
 
+# greetings and brief instructions
 ui.label("Upload File")
-ui.label('(Drag and drop into the box below, or use the "+" button to select a PDF file)')
+ui.label("Use the "+" button to select a PDF file")
+ui.label("If you want to upload another file, please delete the question so the file list may update")
 
 
+# function to append any new file to file_info.csv(contains names and added date of all the files in memory)
+# and duplicate the names in file_info.csv to file_storage
+def update_source_file(name):
+    global file_storage, updated, skippable
+    with open('file_info.csv', mode='a+', newline='') as csv_file:
+        csv_file.seek(0)
+        # check if uploaded file is already stored
+        for row in csv.reader(csv_file):
+            if row[0] == name:
+                ui.notify(message=f"File with the name '{name}' is already in database. Copy the file "
+                                  f"name to use when asking questions", type="info")
+                file_storage.append(row[0])
+                skippable = False
+        # if not in file_info.csv write it to file and append it to file_storage
+        if skippable:
+            csv.writer(csv_file).writerow([name, datetime.datetime.now()])
+            file_storage.append(name)
+    updated = True  # need to check if new file was added or not
+
+
+# function that handles the uploaded file
 def handle_upload(event: events.UploadEventArguments):
-    skipable = True
     ui.notify(f'Uploaded {event.name}')
-    time.sleep(1)
-
+    time.sleep(2)
+    # check if the uploaded file is a PDF format, if not, return warning message and instructions
     if event.type != "application/pdf":
         ui.notify(message="Invalid Document. Your upload is not a PDF file", type="negative",
                   color="red", position="top")
         ui.notify(message="Refresh the page and upload a PDF file", type="info",
                   position="top")
     else:
+        update_source_file(event.name)
+        # if file is not found in file_info.csv write it to memory
+        if skippable:
+            with event.content as f:
+                with open(f"Uploaded Files\{event.name}", "wb") as file:
+                    for line in f.readlines():
+                        file.write(line)
+
+
+# store user input question
+def update_question(e):
+    global question
+    question = e.value
+
+
+# choices for user to select one source from the files in memory
+def source_files():
+    if not updated:
         with open('file_info.csv', mode='a+', newline='') as csv_file:
-            # Check if the file is empty
             csv_file.seek(0)
-            is_file_empty = csv_file.read(1) == ''
-            # Move the file pointer back to the beginning
-            csv_file.seek(0)
-            # Create a CSV reader object and skip header
             csv_reader = csv.reader(csv_file)
-            if not is_file_empty:
-                next(csv_reader)
-            # Check if the first column of any row matches your variable, if not, write the data
-            if skipable:
-                for row in csv_reader:
-                    if row[0] == event.name:
-                        ui.notify(message=f"File with the name '{event.name}' is already in database. Copy the file "
-                                          f"name to use when asking questions", type="info")
-                        skipable = False
-            # Do not write the file name again in the log file
-            if skipable:
-                csv.writer(csv_file).writerow([event.name, datetime.datetime.now()])
-        with event.content as f:
-            with open(f"{event.name}", "wb") as file:
-                for line in f.readlines():
-                    file.write(line)
+            for row in csv_reader:
+                if row[0] not in file_storage:
+                    file_storage.append(row[0])
+    return file_storage
+
+
+# store user input file selection
+def update_selection(e):
+    global select
+    select = e.value
+
+
+# use AI to give answer to user input question
+def AI_answer():
+    if select == "":
+        response = "You did not select the source from the dropdown column \n" + "-"*60
+    else:
+        response = f"Q: {question}  \n" \
+                   f"A: {AI.answer_questions(question, select)}" + "-"*60
+
+    log.push(response)
+
+
+with ui.row():
+    ui.upload(on_upload=handle_upload, auto_upload=True)
+    with ui.row():
+        text_input = ui.input(label='Ask your question', placeholder='Write here', on_change=update_question)
+        with ui.column():
+            ui.label("Select document source").bind_visibility_from(text_input, "value")
+            select_source = ui.select(options=source_files(), with_input=True, on_change=update_selection).\
+                bind_visibility_from(text_input, "value")
+
+    log = ui.log(max_lines=10).classes('w-full h-20')
+    button = ui.button('Find the AI\'s answer', on_click=AI_answer).\
+        bind_visibility_from(text_input, "value")
